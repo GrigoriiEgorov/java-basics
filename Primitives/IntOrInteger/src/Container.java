2dfd3e5d1c3e4c079eab23d61105309ea3e92c40
GrigoriiEgorov <egorov.gr.pav@gmail.com>
2023-04-03 18:02:21 +0200

    init

---
+++ Primitives/IntOrInteger/src/Container.java
@@ -0,0 +1,11 @@
+public class Container {
+    private int count;
+
+    public void addCount(int value) {
+        count = count + value;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
