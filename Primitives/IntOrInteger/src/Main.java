b2b0c8be4737a3fdd524b501b9894b61355bbc06
GrigoriiEgorov <109230410+GrigoriiEgorov@users.noreply.github.com>
2023-04-04 21:08:28 +0200

    Update Main.java

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
-        container.addCount(5672);
+        container.addCount(6527);//5672
         System.out.println(container.getCount());
 
         // TODO: ниже напишите код для выполнения задания:
