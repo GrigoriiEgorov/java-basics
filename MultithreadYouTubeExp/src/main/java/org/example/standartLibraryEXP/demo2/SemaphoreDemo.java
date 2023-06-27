644a11a45b3a6a8a2bb9abf66239db1ebebfaab2
EgorovGrigorii <egorov.gr.pav@gmail.com>
2023-06-27 23:29:44 +0400

    add standartLibraryEXP demo 2

---
+++ MultithreadYouTubeExp/src/main/java/org/example/standartLibraryEXP/demo2/SemaphoreDemo.java
@@ -0,0 +1,54 @@
+package org.example.standartLibraryEXP.demo2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreDemo {
+    public static void main(String[] args) throws Exception{
+        Semaphore semaphore = new Semaphore(2,true);//честная очередь
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            DemoThread thread = new DemoThread(semaphore);
+            threads.add(thread);
+            thread.start();
+        }
+        Thread.sleep(20_000L);
+
+        for(Thread thread : threads){
+            thread.interrupt();
+        }
+    }
+
+    private static class DemoThread extends Thread{
+        private final Semaphore semaphore;
+
+        public DemoThread(Semaphore semaphore){
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try{
+                runUnsafe();
+            }catch (InterruptedException e){
+                System.out.println(getName() + " interrupted");
+            }
+        }
+
+        private void runUnsafe() throws InterruptedException{
+            for(;;){
+                semaphore.acquire();
+                try {
+                    System.out.println(getName() + " acquired semaphore");
+                    Thread.sleep(5000L);
+                } finally {
+                    System.out.println(getName() + " releasing semaphore");
+                    semaphore.release();
+                }
+            }
+        }
+    }
+}
